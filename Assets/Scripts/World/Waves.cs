7c350fb67bdf25c0f5923f7019d999b3ab55e47b
Brendan <bendan1@ymail.com>
2018-05-15 16:21:40 -0500

    Started waves

---
+++ Assets/Scripts/World/Waves.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves : MonoBehaviour {
+
+	public GameObject wave;
+	public float waveDensity = 0.01f;
+	int width = 1200/8;
+	int height = 1200/8;
+
+	void Start () {
+		for(int j = 0; j < height; j++)
+		{
+			for(int i = 0; i < width; i++)
+			{
+				float random = Random.Range(0.0f, 1.0f);
+				if(random < waveDensity)
+				{
+					Vector3 position = new Vector3(i, j, 0.0f);
+					Instantiate(wave, position, Quaternion.identity);
+				}
+			}
+		}
+	}
+	
+
+	void Update () {
+		
+	}
+}
