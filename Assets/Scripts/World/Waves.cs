ff2e83757c19fb05821bf181164294ee6c25380d
Brendan <bendan1@ymail.com>
2018-05-15 19:30:27 -0500

    Another Scene

--- Assets/Scripts/World/Waves.cs
+++ Assets/Scripts/World/Waves.cs
@@ -8,8 +8,10 @@ public class Waves : MonoBehaviour {
 	public float waveDensity = 0.01f;
 	int width = 1200/8;
 	int height = 1200/8;
+	GameObject water;
 
 	void Start () {
+		water = GameObject.FindGameObjectWithTag("Water");
 		for(int j = 0; j < height; j++)
 		{
 			for(int i = 0; i < width; i++)
@@ -18,7 +20,8 @@ public class Waves : MonoBehaviour {
 				if(random < waveDensity)
 				{
 					Vector3 position = new Vector3(i, j, 20.0f);
-					Instantiate(wave, position, Quaternion.identity);
+					GameObject wavePrefab = Instantiate(wave, position, Quaternion.identity);
+					wavePrefab.transform.parent = water.transform;
 				}
 			}
 		}
